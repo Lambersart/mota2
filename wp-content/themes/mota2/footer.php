918b58c9c7be97f6db3715052832046d742679c9
Lambersart <skijek@gmail.com>
2024-06-16 10:13:05 +0200

    mota2

---
+++ wp-content/themes/mota2/footer.php
@@ -0,0 +1,11 @@
+<?php wp_footer(); ?>
+<div class="footer">
+
+    <a href="Mentions légales">Mentions légales</a>
+    <a href="Vie privée">Vie privée</a></li>
+    <a href="Tous droits réservés">Tous droits réservés</a>
+
+</div>
+</body>
+
+</html>
---
+++ wp-content/themes/mota2/index.php
@@ -0,0 +1,20 @@
+<?php get_header(); ?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Nathalie Mota2</title>
+</head>
+
+<body>
+    <h1>Coucou</h1>
+    <div class="imgfront">
+        <img src="/mota2/wp-content/uploads/2024/06/nathalie-11.webp">
+    </div>
+
+</body>
+
+</html>
+<?php get_footer(); ?>
